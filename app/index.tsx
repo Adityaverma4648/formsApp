773bd3a294f46260f738b3256770629141265f60
Aditya verma <adityaverma4648@gmail.com>
2024-12-11 14:37:38 +0530

    NativeWind+Tailwindcss completed

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View
+
+      className="bg-orange-300"
+
+      // style={{
+      //   flex: 1,
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      // }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
